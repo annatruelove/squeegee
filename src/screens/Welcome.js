f230eab86f8bfb90ce891823c85a6368239ef5ac
Anna Truelove <anna97@live.unc.edu>
2020-02-22 17:09:45 -0500

    bare welc screen done

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -4,25 +4,45 @@ import '../App.css';
 import '../custom.scss';
 import { NavLink } from 'react-router-dom';
 import squeegee from '../squeegee.png';
+import profile_icon from '../profile.svg';
+import search_icon from '../search.svg';
 
-const Welcome = () => {    
-        return ( 
-          <NavLink to="/details">
-              <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav-bar">
-                    <p class="navbar-brand" id="p">Squeegee</p>
-                    <form class="form-inline my-2 my-lg-0" >
-                        <input class="form-control mr-sm-2" id="welcome-search" type="search" placeholder="Search Jobs or Companies" aria-label="Search"></input>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </nav>
 
-                <p>Welcome to Squeegee!</p>
-                <img src={squeegee}></img>
-            <Button className='btn-flat'>
-              Hello
-            </Button>
-          </NavLink>
-        );
-      }
+
+const Welcome = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav-bar">
+                <img src={squeegee} alt="logo" id="nav-logo"></img>
+                <img src={search_icon} alt="logo" id="nav-logo"></img>
+                <form class="form-inline my-2 my-lg-0" >
+                    <input class="form-control mr-sm-2" id="welcome-search" type="search" placeholder="Search Jobs or Companies" aria-label="Search"></input>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <img src={profile_icon} alt="logo" id="nav-logo"></img>
+
+            </nav>
+
+            <p>Welcome to Squeegee!</p>
+           
+
+                <div class="welcome">
+                <NavLink to="/details">
+                    <Button className='btn-flat' id="welc-btn">
+                        Sign Up
+                    </Button>
+                </NavLink>
+
+                <NavLink to="/details">
+                    <Button className='btn-flat' id="welc-btn">
+                        Sign In
+                    </Button>
+                </NavLink>
+
+                </div>
+
+        </div>
+    );
+}
 
 export default Welcome;
