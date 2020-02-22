b0e4884d73954e791739a24a5ecef3e80b0c3b80
Katherine Griffin <kag728@gmail.com>
2020-02-22 14:54:39 -0500

    adds Bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './screens/Welcome';
 
 class App extends Component {
