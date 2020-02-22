d3583ef2004dd3aab4ab60b6cd4fdc43ba15873a
Anna Truelove <anna97@live.unc.edu>
2020-02-22 18:54:10 -0500

    home page is set up

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -14,18 +14,25 @@ const Home = () => {
             <Navigation />
 
             <div class="home">
+
                 <div id="row">
                     <img src={company_icon} alt="logo" id="home-logo"></img>
                     <p>Companies</p>
+                    <p>Browse through companies to view personal reviews and ratings.</p>
                 </div>
+
                 <div id="row">
                     <img src={job_icon} alt="logo" id="home-logo"></img>
                     <p>Jobs</p>
+                    <p>Search for jobs you're interested in to learn about their interview process, compensation, and other important aspects. </p>
                 </div>
+
                 <div id="row">
                     <img src={budget_icon} alt="logo" id="home-logo"></img>
                     <p>Budget</p>
+                    <p>Use our budgeting tool to compare cost of living with your potential salary and desired location.</p>
                 </div>
+
             </div>
         </div>
 
