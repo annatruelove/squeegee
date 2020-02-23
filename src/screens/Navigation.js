e97acbe0d307c14a9327de7055279d8e2c000dcd
Sarah Bost <sbost99@live.unc.edu>
2020-02-23 01:23:45 -0500

    Added links to nav bar

--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -9,6 +9,9 @@ const Navigation = () => {
             <NavLink to="/">
               <img src={squeegee} alt="logo" id="squeegee-logo"></img>
             </NavLink>
+            <NavLink to="./companies">Companies</NavLink>
+            <NavLink to="./jobs">Jobs</NavLink>
+            <NavLink to="./budget">Budget</NavLink>
               <div id='search-bar'>
                 <form class="form-inline my-2 my-lg-0" >
                     <input class="form-control mr-sm-2" id="welcome-search" type="search" placeholder="🔍 Search Jobs or Companies" aria-label="Search"></input>
@@ -20,6 +23,8 @@ const Navigation = () => {
               <NavLink to="/profile">
                 <img src={profile_icon} alt="logo" id="user-logo"></img>
               </NavLink>
+             
+
 
           </nav>
     )
