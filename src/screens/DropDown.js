f2f3618a5a21dc64c526e6b72c0c3b881ad1e4d9
Anna Truelove <anna97@live.unc.edu>
2020-02-22 22:50:15 -0500

    failure

--- src/screens/DropDown.js
+++ src/screens/DropDown.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 const DropDown = (props) => {
+
     const items = [];
     for (const [i, x] of props.items.entries()) {
         items.push({ label: x, value: i });
@@ -15,12 +14,13 @@ const DropDown = (props) => {
             <div className="row">
                 <div className="col-md-4"></div>
                 <div className="col-md-4">
-                    <Select options={items} />
+                    <Select options={items}/>
                 </div>
                 <div className="col-md-4"></div>
             </div>
         </div>
     );
+
 }
 
 export default DropDown;
